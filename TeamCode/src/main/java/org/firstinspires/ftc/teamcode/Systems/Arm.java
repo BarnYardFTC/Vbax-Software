aefc9cfb849a6992bbe4630ca13877fc20912735
Egor <go1603ra@gmail.com>
2025-06-11 12:49:50 +0300

    created  all fields that arm class has, trying to calculate start degree

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Arm.java
@@ -7,14 +7,20 @@ public class Arm {
 
     private DcMotor armRight;
     private DcMotor armLeft;
-    private int angle;
-    private final int minSoftLim = 45;
-    private final int maxSoftLim = 315;
-    private enum armPositions{
-        COLLECTION,
-        SCORE,
+    private final double startDegree = 35;
+    private double angle;
+    public static int minSoftLim = 45;
+    public static int maxSoftLim = 315;
 
-    }
+    public static int DEFAULT = 180;
+    public static int SUB_PREP = 100;
+    public static int SUB_COLLECT = 80;
+    public static int COLLECT_FIELD_FRONT = 140;
+    public static int COLLECT_FIELD_BACK = 220;
+    public static int SCORE_LB = 250;
+    public static int PREP_SPEC = 190;
+    public static int SCORE_SPEC = 210;
+    public static int CLIMB = 300;
     /*
     we calculate the ratio of between the arm ticks and the degrees of rotation in the arm.
      */
@@ -24,6 +30,16 @@ public class Arm {
     private final double TICK_PER_DEGREE = OUTER_GEAR_RATIO * INNER_MOTOR_GEAR_RATIO * ENCODERS_PER_INNER_ROTATION / 360;
 
     // preset, func, controls
+
+    public Arm(DcMotor left, DcMotor right){
+
+        this.armLeft = left;
+        this.armRight = right;
+
+        this.angle = startDegree;
+
+    }
+
 
 
 
